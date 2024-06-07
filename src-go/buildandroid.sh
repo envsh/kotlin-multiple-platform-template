acb5c690552ea5144c4d885f51dd6b836454c53f
envsh <yatsen21@outlook.com>
2024-06-07 21:48:51 +0800

    clrsom

--- src-go/buildandroid.sh
+++ src-go/buildandroid.sh
@@ -6,16 +6,16 @@ set -e fail
 
 NDKDIR=/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64
 
-export CGO_CFLAGS="-I/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include -I/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/sysroot/usr/include/arm-linux-androideabi"
+export CGO_CFLAGS="-I${NDKDIR}/sysroot/usr/include -I${NDKDIR}/sysroot/usr/include/arm-linux-androideabi"
 
 
-# export CC=/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/bin/armv7a-linux-androideabi21-clang
-export CC=/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang
+# export CC=${NDKDIR}/bin/armv7a-linux-androideabi21-clang
+export CC=${NDKDIR}/bin/aarch64-linux-android21-clang
 RUSTUP_TOOLCHAIN=$CC
 
-# export CC=/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/bin/aarch64-linux-android21-clang
+# export CC=${NDKDIR}/bin/aarch64-linux-android21-clang
 
-# export LD=/usr/local/Caskroom/android-ndk/26d/AndroidNDK11579264.app/Contents/NDK//toolchains/llvm/prebuilt/darwin-x86_64/bin/ld
+# export LD=${NDKDIR}/bin/ld
 
 #GOOS=android  GOARCH=arm64  CGO_LDFLAGS="-arch arm64" CGO_ENABLED=1 go build -buildmode=c-shared -o ../fedimuigo.so
 GOOS=android  GOARCH=arm64  CGO_CFLAGS="-arch arm64" CGO_LDFLAGS="-arch arm64" CGO_ENABLED=1 go build -buildmode=c-shared -o ../jniLibs/libhellogo.so
